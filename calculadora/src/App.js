3c6c86f9a184df9ad1bb1c20632b57adbd2f1185
Alejandro López Pérez <60120226+alejo117lp@users.noreply.github.com>
2023-12-21 11:37:09 -0500

    init Calculator

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import fundaLogo from './img/funda-logo.png';
+import Boton from './componentes/Boton';
 
 function App() {
   return (
     <div className="App">
+      <div className='funda-logo-contenedor'>
+        <img src={fundaLogo}
+          className='funda-logo'
+          alt='Logo Funda'/>
+      </div>
+      <div className='contenedor-calculadora'>
+        <div className='fila'>
+          <Boton>1</Boton>
+        </div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+      </div>
       
     </div>
   );
---
+++ calculadora/src/componentes/Boton.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Boton(props){
+    return (
+        <div 
+         className={`boton-contenedor`}>
+            {props.children}
+        </div>
+    );
+}
+
+export default Boton;
