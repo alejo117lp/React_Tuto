8de0170f897a75bdbac27ec42d85f87c2f286239
Alejandro López Pérez <60120226+alejo117lp@users.noreply.github.com>
2023-12-26 09:52:01 -0500

    comentarios p1

--- app-tareas/src/Componentes/Tarea.js
+++ app-tareas/src/Componentes/Tarea.js
@@ -4,6 +4,12 @@ import { AiOutlineCloseCircle } from "react-icons/ai";
 
 function Tarea( {id,texto, completada,completarTarea, eliminarTarea} ){
     return(
+
+        /*Div con dos contenedores, uno para el texto de la tarea, otro para el icono de eliminar
+        Al hacer clic en el primero se llama a la funcion completar tarea (ep id especifica
+            cual tarea). En el segundo se llama a la función eliminar tarea y se usa el paquete
+            de iconos de React, en este caso  AiOutlineCloseCircle, allí podría ir también
+            la <img /> */
         <div className={completada ? 'tarea-contenedor completada' : 'tarea-contenedor'}>
             <div className="tarea-texto"
                 onClick={() => completarTarea(id)}>
